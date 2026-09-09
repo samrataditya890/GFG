f9b7a7a993bbca44d3bb6e38609cde3f228efc00
Aditya Gupta <adityagupta20215@gmail.com>
2026-09-09 14:51:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pass by Reference and Value/pass-by-reference-and-value.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    vector<int> passedBy(int a, int &b) {
+        // code here
+        vector<int> ans;
+        ans.push_back(a+1);
+        ans.push_back(b+2);
+        
+        return ans;
+    }
+};
